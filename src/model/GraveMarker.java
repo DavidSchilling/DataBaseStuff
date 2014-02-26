fb8c241f5239eeb26958ce855bee8c14fe0ab673
DavidSchilling <desideriosemper@gmail.com>
2014-02-26 08:46:12 -0700

    Cleaned up Code

--- src/model/GraveMarker.java
+++ src/model/GraveMarker.java
@@ -4,38 +4,100 @@ import java.util.ArrayList;
 
 public class GraveMarker
 {
-	//type of marker
-	//picture
-	//location
-	//people <>
-	//information
-	//isLegible
-	
+	// type of marker
+	// picture
+	// location
+	// people <>
+	// information
+	// isLegible
+
 	private String markerType;
 	private ArrayList<Person> gravePeople;
 	private boolean isLegible;
 	private String moreInformation;
-	
-	
+
+	public GraveMarker()
+	{
+		markerType = "";
+		gravePeople = new ArrayList<Person>();
+		isLegible = false;
+		moreInformation = "";
+	}
+
+	public GraveMarker(String markerType, ArrayList<Person> gravePeople, boolean isLegible,
+			String moreInformation)
+	{
+		this.markerType = markerType;
+		this.gravePeople = gravePeople;
+		this.isLegible = isLegible;
+		this.moreInformation = moreInformation;
+	}
+
+	/* ********************* GETTERS *********************** */
+	public String getMarkerType()
+	{
+		return markerType;
+	}
+
+	public ArrayList<Person> getGravePeople()
+	{
+		return gravePeople;
+	}
+
+	public boolean isLegible()
+	{
+		return isLegible;
+	}
+
+	public String getMoreInformation()
+	{
+		return moreInformation;
+	}
+
+	/* ******************** SETTERS *********************** */
+	public void setMarkerType(String markerType)
+	{
+		this.markerType = markerType;
+	}
+
+	public void setGravePeople(ArrayList<Person> gravePeople)
+	{
+		this.gravePeople = gravePeople;
+	}
+
+	public void setLegible(boolean isLegible)
+	{
+		this.isLegible = isLegible;
+	}
+
+	public void setMoreInformation(String moreInformation)
+	{
+		this.moreInformation = moreInformation;
+	}
+
+	/**
+	 * @return graveInfo Names and ages of the people buried, whether or not the
+	 *         marker is legible, and other information.
+	 */
 	public String toString()
 	{
 		String graveInfo = "";
-		
-		for(Person current : gravePeople)
+
+		for (Person current : gravePeople)
 		{
-			graveInfo += current.toString() + " is buried here.\n";
+			graveInfo += current + " is buried here.\n";
 		}
-		if(isLegible)
+		if (isLegible)
 		{
 			graveInfo += "This marker is legible.\n";
-		}
-		else
+		} else
 		{
 			graveInfo += "This marker is NOT legible.\n";
 		}
-		
-		
+
+		graveInfo += moreInformation;
+
 		return graveInfo;
 	}
-	
+
 }
--- src/model/Person.java
+++ src/model/Person.java
@@ -6,14 +6,60 @@ public class Person
 	private int age;
 	private String birthDate;
 	private String deathDate;
-	
-	
+
+	/* ****************** GETTERS ******************* */
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public String getBirthDate()
+	{
+		return birthDate;
+	}
+
+	public String getDeathDate()
+	{
+		return deathDate;
+	}
+
+	/* ******************* SETTERS ******************** */
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public void setBirthDate(String birthDate)
+	{
+		this.birthDate = birthDate;
+	}
+
+	public void setDeathDate(String deathDate)
+	{
+		this.deathDate = deathDate;
+	}
+
+	/**
+	 * 
+	 * @return personInfo, which contains the name and age of a person.
+	 */
 	public String toString()
 	{
 		String personInfo = "";
-		
+
 		personInfo += name + ", aged " + age;
-		
+
 		return personInfo;
 	}
 
